93a8001df5861c57d206e0bed8a919bd600f1944
louisreberga <louis.reberga@gmail.com>
2022-04-12 14:41:47 +0200

    dropout

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ def cnn(emb_matrix, emb_dim):
 def lstm(emb_matrix, emb_dim):
     model = Sequential()
     model.add(Embedding(VOCAB_SIZE, emb_dim, weights=[emb_matrix], input_length=MAX_LENGTH, trainable=False))
-    model.add(LSTM(60, return_sequences=True))
+    model.add(LSTM(50, return_sequences=True))
     model.add(GlobalMaxPool1D())
     model.add(Dropout(0.1))
     model.add(Dense(50, activation="relu"))
@@ -27,8 +27,9 @@ def lstm(emb_matrix, emb_dim):
 def bidirectional_lstm(emb_matrix, emb_dim):
     model = Sequential()
     model.add(Embedding(VOCAB_SIZE, emb_dim, weights=[emb_matrix], input_length=MAX_LENGTH, trainable=False))
-    model.add(Bidirectional(LSTM(60, return_sequences=True, dropout=0.1, recurrent_dropout=0.1)))
+    model.add(Bidirectional(LSTM(50, return_sequences=True, dropout=0.1, recurrent_dropout=0.1)))
     model.add(GlobalMaxPool1D())
+    model.add(Dropout(0.1))
     model.add(Dense(50, activation="relu"))
     model.add(Dropout(0.1))
     model.add(Dense(6, activation="sigmoid"))
